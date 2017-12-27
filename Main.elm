0bde82b48c63a38f9432876acbd0daeece15e034
Erik Dymbe <erik.dymbe@gmail.com>
2017-12-27 01:37:01 +0100

    Make a simple elm-app for (safely) entering an integer in an input field, and receiving a reply

--- Main.elm
+++ Main.elm
@@ -1,40 +1,85 @@
 module Main exposing (..)
 
+import Debug exposing (log)
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 
 type alias Model =
-    Int
+    { inputText : String
+    , outputText : String
+    }
+
+
+model : Model
+model =
+    { inputText = ""
+    , outputText = ""
+    }
 
 
 type Msg
-    = Increment
-    | Decrement
+    = ChangeInputText String
+    | ChangeOutputText
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        ChangeInputText newInput ->
+            log "InputChange"
+                { model | inputText = newInput }
 
-        Decrement ->
-            model - 1
+        ChangeOutputText ->
+            log "ChangeOutputText"
+                { model | outputText = getOutputText model.inputText }
+
+
+toInt : String -> Int
+toInt str =
+    let
+        result =
+            String.toInt str
+    in
+    case result of
+        Ok v ->
+            v
+
+        Err e ->
+            0
+
+
+getOutputText : String -> String
+getOutputText inputText =
+    let
+        result =
+            String.toInt inputText
+    in
+    case result of
+        Ok i ->
+            "You have " ++ toString i ++ " something!"
+
+        Err e ->
+            "Invalid input!"
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Increment ] [ text "+" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick Decrement ] [ text "-" ]
+        [ input
+            [ placeholder "Enter amount..."
+            , onInput ChangeInputText
+            ]
+            []
+        , button [ onClick ChangeOutputText ] [ text "Change amount" ]
+        , div [] [ text model.outputText ]
         ]
 
 
 main =
     beginnerProgram
-        { model = 0
+        { model = model
         , view = view
         , update = update
         }
