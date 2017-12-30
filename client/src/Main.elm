d4ec0f6b0feac813f39cb4f90903c1896c86ae4a
Erik Dymbe <erik.dymbe@gmail.com>
2017-12-30 21:16:45 +0100

    restructure Main

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -4,50 +4,104 @@ import Debug exposing (log)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode exposing (..)
+
+
+main =
+    program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
 
 
 type alias Model =
-    { inputText : String
-    , outputText : String
+    { cryptoName : Maybe String
+    , cryptoAmount : Maybe Float
+    , fiat : Maybe String
+    , cryptoPrice : Maybe Float
+    , output : String
     }
 
 
-model : Model
-model =
-    { inputText = ""
-    , outputText = ""
-    }
+init : ( Model, Cmd Msg )
+init =
+    ( { cryptoName = Nothing
+      , cryptoAmount = Nothing
+      , fiat = Nothing
+      , cryptoPrice = Nothing
+      , output = ""
+      }
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
 
 
 type Msg
-    = ChangeInputText String
-    | ChangeOutputText
+    = ChangeCryptoName String
+    | ChangeCryptoAmount String
+    | ChangeFiat String
+    | GetCryptoPrice
+    | NewPrice (Result Http.Error String)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ChangeInputText newInput ->
-            log "InputChange"
-                { model | inputText = newInput }
+        ChangeCryptoName newName ->
+            ( { model | cryptoName = Just newName }, Cmd.none )
 
-        ChangeOutputText ->
-            log "ChangeOutputText"
-                { model | outputText = getOutputText model.inputText }
+        ChangeCryptoAmount newAmount ->
+            ( { model | cryptoAmount = stringToMaybeFloat newAmount }, Cmd.none )
+
+        ChangeFiat newFiat ->
+            ( { model | fiat = Just newFiat }, Cmd.none )
+
+        GetCryptoPrice ->
+            ( model, getCryptoPrice model.cryptoName (stringOrEmpty model.fiat) )
+
+        NewPrice (Ok newPrice) ->
+            ( { model | cryptoPrice = stringToMaybeFloat newPrice }, Cmd.none )
+
+        NewPrice (Err e) ->
+            let
+                a =
+                    log "error" e
+            in
+            ( model, Cmd.none )
 
 
-toInt : String -> Int
-toInt str =
+stringOrEmpty : Maybe String -> String
+stringOrEmpty str =
+    case str of
+        Just s ->
+            s
+
+        Nothing ->
+            ""
+
+
+stringToMaybeFloat : String -> Maybe Float
+stringToMaybeFloat str =
     let
         result =
-            String.toInt str
+            String.toFloat str
     in
     case result of
-        Ok v ->
-            v
+        Ok f ->
+            Just f
 
-        Err e ->
-            0
+        Err _ ->
+            Nothing
 
 
 getOutputText : String -> String
@@ -64,22 +118,76 @@ getOutputText inputText =
             "Invalid input!"
 
 
+getCryptoPrice : Maybe String -> String -> Cmd Msg
+getCryptoPrice crypto fiat =
+    case crypto of
+        Nothing ->
+            Cmd.none
+
+        Just crypto ->
+            let
+                url =
+                    "https://api.coinmarketcap.com/v1/ticker/"
+                        ++ crypto
+                        ++ "/?convert="
+                        ++ fiat
+
+                request =
+                    Http.get url (decodeCryptoPrice fiat)
+            in
+            Http.send NewPrice request
+
+
+decodeCryptoPrice : String -> Decoder String
+decodeCryptoPrice fiat =
+    index 0 (field ("price_" ++ String.toLower fiat) string)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ input
-            [ placeholder "Enter amount..."
-            , onInput ChangeInputText
-            ]
-            []
-        , button [ onClick ChangeOutputText ] [ text "Change amount" ]
-        , div [] [ text model.outputText ]
+        [ inputRow "Crypto name:" "e.g. bitcoin, ripple..." ChangeCryptoName
+        , inputRow "Crypto amount:" "Enter amount..." ChangeCryptoAmount
+        , inputRow "Convert to fiat:" "e.g. USD, EUR..." ChangeFiat
+        , div [] [ text model.output ]
         ]
 
 
-main =
-    beginnerProgram
-        { model = model
-        , view = view
-        , update = update
-        }
+inputRow : String -> String -> (String -> Msg) -> Html Msg
+inputRow label placeholderText msg =
+    div []
+        [ span [] [ text label ]
+        , input
+            [ placeholder placeholderText, onInput msg ]
+            []
+        ]
+
+
+
+{-
+   view model =
+       div []
+           [ input
+               [ placeholder "Enter amount..."
+               , onInput ChangeInputText
+               ]
+               []
+           , button [ onClick ChangeOutputText ] [ text "Change amount" ]
+           , div [] [ text model.outputText ]
+           , button [ onClick GetCryptoPrice ] [ text ("Get " ++ model.cryptoName ++ " price") ]
+           , div [] [ text model.cryptoPrice ]
+           ]
+-}
